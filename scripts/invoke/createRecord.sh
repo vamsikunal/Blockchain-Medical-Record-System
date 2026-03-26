ec70ef1e14c096130f26217e4820e60c17b42f49
Kanishk Agarwal <kanishkagarwal208@gmail.com>
2026-03-26 13:12:09 +0530

    add createRecord to invoke CreatePatientRecord

--- scripts/invoke/createRecord.sh
+++ scripts/invoke/createRecord.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+source "$(dirname "$0")/../env.sh"
+peer chaincode invoke \
+  -o $ORDERER \
+  -C $CHANNEL \
+  -n $CHAINCODE \
+  --peerAddresses $PEER_HOSPITAL \
+  -c "{\"function\":\"CreatePatientRecord\",\"Args\":[\"$1\",\"$2\",\"$3\",\"$4\"]}"
+# Usage: ./createRecord.sh <patientId> <name> <age> <diagnosis>
+# Must be called with HospitalMSP peer context
