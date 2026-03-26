6da60e2cff2bf56ca6b5378c1702d0f469ad8a65
Kanishk Agarwal <kanishkagarwal208@gmail.com>
2026-03-26 09:48:21 +0530

    init(scripts) add env.sh and empty script placeholders

---
+++ scripts/env.sh
@@ -0,0 +1,7 @@
+export CHANNEL=medicalchannel
+export CHAINCODE=medicalrecord
+export ORDERER=orderer.example.com:7050
+export PEER_HOSPITAL=peer0.hospital.com:7051
+export PEER_DOCTOR=peer0.doctor.com:7051
+export PEER_PATIENT=peer0.patient.com:7051
+export CORE_PEER_TLS_ENABLED=false
