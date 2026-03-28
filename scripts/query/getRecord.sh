f6e25b40087e3df0dcef59d42a874f742952751d
Kanishk Agarwal <kanishkagarwal208@gmail.com>
2026-03-28 10:22:14 +0530

    add query script for GetPatientRecord

--- scripts/query/getRecord.sh
+++ scripts/query/getRecord.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+source "$(dirname "$0")/../env.sh"
+peer chaincode query \
+  -C $CHANNEL \
+  -n $CHAINCODE \
+  -c "{\"function\":\"GetPatientRecord\",\"Args\":[\"$1\"]}"
+# Usage: ./getRecord.sh <patientId>
+# Must be called with PatientMSP peer context
